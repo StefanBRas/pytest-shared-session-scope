d04511d4b28eec83be0a407063d5d1dd9ab2f568
Stefan Rasmussen <sras@wayfare.ai>
2024-08-20 15:54:53 +0200

    chore: try stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 from pytest_shared_session_scope import shared_json_scope_fixture
 import pytest
+from _pytest.fixtures import FixtureManager
 
 
 pytest_plugins = ["pytester"]
@@ -16,3 +17,12 @@ def tmp_fixture():
 
 def _my_fixture_with_worker_id_storage(worker_id):
     yield worker_id
+
+
+def pytest_collection_modifyitems(session: pytest.Session, config: pytest.Config, items: list[pytest.Item]):
+    fixture_manager = session.config.pluginmanager.get_plugin("funcmanage")
+    assert isinstance(fixture_manager, FixtureManager)
+    for item in items:
+        item.keywords
+
+
