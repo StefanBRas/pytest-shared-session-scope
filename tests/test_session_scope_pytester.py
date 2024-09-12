4837d9893f2c78b195004ea4e4d4a8ecd97400ac
StefanBRas <gitcommits@bruhn.io>
2024-09-12 21:37:40 +0200

    testing,doc: add cache example

--- tests/test_session_scope_pytester.py
+++ tests/test_session_scope_pytester.py
@@ -41,19 +41,19 @@ def get_output_dir(path: Path) -> Path:
     return result_path
 
 
-@pytest.mark.parametrize("n", [0, 2, 5])
+@pytest.mark.parametrize("n", [0, 2, 3])
 def test_with_yield(pytester: Pytester, tmp_path, n: int):
     copy_example(pytester, "with_yield", tmp_path)
     pytester.runpytest("-n", str(n), "--basetemp", str(tmp_path)).assert_outcomes(passed=5)
 
 
-@pytest.mark.parametrize("n", [0, 2, 5])
+@pytest.mark.parametrize("n", [0, 2, 3])
 def test_with_return(pytester: Pytester, n: int, tmp_path: Path):
     pytester.copy_example("with_return")
     pytester.runpytest("-n", str(n), "--basetemp", str(tmp_path)).assert_outcomes(passed=5)
 
 
-@pytest.mark.parametrize("n", [0, 2, 5])
+@pytest.mark.parametrize("n", [0, 2, 3])
 def test_with_cleanup(pytester: Pytester, n: int, tmp_path):
     test_id = "with_cleanup"
     copy_example(pytester, test_id, tmp_path)
@@ -76,27 +76,34 @@ def test_with_cleanup(pytester: Pytester, n: int, tmp_path):
     assert len(not_last) == max(n - 1, 0)
 
 
-@pytest.mark.parametrize("n", [0, 2, 5])
+@pytest.mark.parametrize("n", [0, 2, 3])
 def test_serialize(pytester: Pytester, n: int, tmp_path: Path):
     pytester.copy_example("test_serializer.py")
     pytester.runpytest("-n", str(n), "--basetemp", str(tmp_path)).assert_outcomes(passed=8)
 
 
-@pytest.mark.parametrize("n", [0, 2, 5])
+@pytest.mark.parametrize("n", [0, 2, 3])
 def test_parse(pytester: Pytester, n: int, tmp_path: Path):
     pytester.copy_example("test_parse.py")
     pytester.runpytest("-n", str(n), "--basetemp", str(tmp_path)).assert_outcomes(passed=8)
 
 
-@pytest.mark.parametrize("n", [0, 2, 5])
+@pytest.mark.parametrize("n", [0, 2, 3])
 def test_use_fixture_in_fixture(pytester: Pytester, n: int, tmp_path: Path):
     pytester.copy_example("test_use_fixture_in_pytest_fixture.py")
     pytester.runpytest("-n", str(n), "--basetemp", str(tmp_path)).assert_outcomes(passed=3)
 
 
-@pytest.mark.parametrize("n", [0, 2, 5])
+@pytest.mark.parametrize("n", [0, 2, 3])
 def test_custom_store(pytester: Pytester, n: int, tmp_path: Path):
     pytester.copy_example("test_custom_store.py")
+    pytester.runpytest("-n", str(n), "--basetemp", str(tmp_path)).assert_outcomes(passed=3)
+
+@pytest.mark.parametrize("n", [0, 2, 3])
+def test_cache(pytester: Pytester, n: int, tmp_path: Path):
+    pytester.copy_example("test_cache.py")
+    pytester.runpytest("-n", str(n), "--basetemp", str(tmp_path)).assert_outcomes(passed=3)
+    # TODO: assert that we actually use the cache
     pytester.runpytest("-n", str(n), "--basetemp", str(tmp_path)).assert_outcomes(passed=3)
 
 
