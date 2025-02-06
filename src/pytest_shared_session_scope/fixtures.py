8ea41d6ec9e0c62132194ba152036c992f0f70f1
StefanBRas <gitcommits@bruhn.io>
2025-02-06 20:48:13 +0100

    chore: cleanup

--- src/pytest_shared_session_scope/fixtures.py
+++ src/pytest_shared_session_scope/fixtures.py
@@ -172,11 +172,10 @@ def shared_session_scope_fixture(
                         json.loads(metadata_storage.read(metadata_identifier, fixture_values))
                     )
                     tests_missing -= set(tests_run_in_worker)
-                    print(f"The following tests are missing: {tests_missing}")
                     is_last = not tests_missing
+                    # TODO: The fixture that closes shouldn't need to write here
+                    # But there are issue with getfixturevalue.
                     # if not is_last:
-                    # TODO: The fixture to close shouldn't need to write here
-                    # But there are issue with getfixturevalue
                     metadata_storage.write(
                         metadata_identifier,
                         json.dumps(list(tests_missing)),
